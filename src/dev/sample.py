fcf27a52915dc82ccb84769e9a7d202029fff390
weponpon <umitaku7@gmail.com>
2023-02-05 19:43:58 +0900

    streamlitのサンプルを追加

---
+++ src/dev/sample.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+"""
+## [Streamlit](https://docs.streamlit.io/)ではマークダウンを使用できます
+###この記法の詳細は[こちら](https://docs.streamlit.io/library/api-reference/write-magic/magic)
+他にも[API reference](https://docs.streamlit.io/library/api-reference)に色んな機能が載っています
+```python
+print("hello world!")
+"""
+
+st.title("Hello World")
